26a58dab8174011330acf7729c698d0bac204f41
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2023-02-15 19:11:42 +0200

    frontend: feat: implement ship context

---
+++ frontend-react/src/utilities/ShipContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useState } from "react";
+
+export const ShipContext = React.createContext();
+
+const ShipProvider = ({ children }) => {
+  const [shipment, setShipment] = useState({});
+
+  return (
+    <ShipContext.Provider value={{ shipment, setShipment }}>
+      {children}
+    </ShipContext.Provider>
+  );
+};
+
+export const useShipment = () => {
+  const { shipment, setShipment } = React.useContext(ShipContext);
+  return { shipment, setShipment };
+};
+
+export default ShipProvider;
