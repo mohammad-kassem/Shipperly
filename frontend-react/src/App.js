a9869fe9e2be2f7563cb9919d1b7299e6efaec57
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2023-02-15 19:12:07 +0200

    frontend: feat: include all routes in App

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddShip from "./pages/AddShip";
+import EditShip from "./pages/EditShip";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Shipments from "./pages/Shipments";
+import ShipProvider from "./utilities/ShipContext";
 
 function App() {
   return (
@@ -8,8 +12,14 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/"></Route>
       </Routes>
+      <ShipProvider>
+        <Routes>
+          <Route path="/" element={<Shipments />}></Route>
+          <Route path="/add" element={<AddShip />}></Route>
+          <Route path="/edit" element={<EditShip />}></Route>
+        </Routes>
+      </ShipProvider>
     </BrowserRouter>
   );
 }
